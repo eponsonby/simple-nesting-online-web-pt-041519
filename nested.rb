6558d18aabdbab1fe45843fc997a40ee195a987e
Erin Ponsonby <erinponsonby@gmail.com>
2019-04-22 22:31:59 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,8 +83,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-programmer_hash[:yukihiro_matsumoto] = :known_for
+programmer_hash[:yukihiro_matsumoto] = {}
+programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
 programmer_hash[:yukihiro_matsumoto][:Languages] = ["LISP", "C"]
 
 return programmer_hash
